f32a43c5081e6674b885cd88e20016315c434979
arunEddy <36036729+arunEddy@users.noreply.github.com>
2025-12-22 20:41:07 +0530

    DALClass

---
+++ CountryMaster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALCLASS
+{
+    public  class CountryMaster
+    {
+        [Key]
+        public int CountryId { get; set; }
+        [Required]  
+        public string CountryName { get; set; }
+
+    }
+}
